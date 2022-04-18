9040c847280bc8b888d08b8b0fec8af00923700f
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-04-18 23:18:37 +0500

    first steps websockets

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import io from 'socket.io-client';
+
+const socketProtocol = (window.location.protocol.includes('https')) ? 'wss' : 'ws';
+const socket = io(`${socketProtocol}://${window.location.host}`, { reconnection: false });
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const { Server } = require('socket.io')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Welcome to cookie.io')
-})
+app.use('/static', express.static('public'))
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const io = new Server(server);
+io.on('connection', socket => {
+    console.log('Player connected!', socket.id);
+});
