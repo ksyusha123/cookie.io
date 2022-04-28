59dcee9ee793d6968c0766b2d08a4ec1488f901f
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-04-28 12:45:01 +0500

    rmp

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,10 @@
-// import {play} from "./client";
+import io from 'socket.io-client';
+import { throttle } from 'throttle-debounce';
+
+const socket = io();
 
 const play = (username) => socket.emit('join', username);
+export const updateDirection = throttle(100, direction => socket.emit('update', direction));
 
 document.getElementById("play-button").onclick = () => {
     console.log(1);
