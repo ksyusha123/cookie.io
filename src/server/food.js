b2dba42378a5747a12db0130eb52649d9c1447bf
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-05-12 18:33:12 +0500

    add food full

--- src/server/food.js
+++ src/server/food.js
@@ -1,9 +1,12 @@
 const GameObject = require('./gameObject');
 const settings = require('../settings');
 
+import {generateId} from "./utils";
 
-class Food extends GameObject {
-    constructor(id) {
+
+export class Food extends GameObject {
+    constructor() {
+        const id = generateId();
         const x = settings.MAP_SIZE * Math.random();
         const y = settings.MAP_SIZE * Math.random();
         super(id, x, y, settings.FOOD_RADIUS);
