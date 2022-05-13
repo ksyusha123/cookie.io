e338b3164a79e06f69666f1ae5fb1be72fd90321
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-05-13 20:32:26 +0500

    fix

--- src/server/food.js
+++ src/server/food.js
@@ -1,12 +1,13 @@
 const GameObject = require('./gameObject');
 const settings = require('../settings');
+const getRandomIntFromRange = require('./utils');
 
 
 class Food extends GameObject {
     constructor() {
         const id = Date.now();
-        const x = settings.MAP_SIZE * Math.random();
-        const y = settings.MAP_SIZE * Math.random();
+        const x = getRandomIntFromRange(0, settings.MAP_SIZE);
+        const y = getRandomIntFromRange(0, settings.MAP_SIZE);
         super(id, x, y, settings.FOOD_RADIUS);
     }
 }
