52ed38e706607e65c02555f438139fff83835ecc
Polinariaa <polina.shch2@mail.ru>
2022-05-17 15:37:13 +0500

    choosing skin begining

--- src/client/index.js
+++ src/client/index.js
@@ -15,6 +15,20 @@ document.getElementById("play-button").onclick = () => {
     startCapturingInput();
 };
 
+document.getElementById("select-skin-button").onclick = () => {
+    console.log("click on select button");
+    document.getElementsByClassName('menu')[0].style.display = 'none';
+    document.getElementsByClassName('chose-menu')[0].style.display = 'none';
+    document.getElementById("modal").style.display = 'flex';
+};
+
+document.getElementById("modal__close-button").onclick = () => {
+    console.log("click on back select button");
+    document.getElementsByClassName('menu')[0].style.display = 'flex';
+    document.getElementsByClassName('chose-menu')[0].style.display = 'flex';
+    document.getElementById("modal").style.display = 'none';
+};
+
 function onMouseInput(e) {
     handleInput(e.clientX, e.clientY);
 }
