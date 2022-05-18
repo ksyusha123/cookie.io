5cad4d3039f2717b9e8e87d33092617fc89df1b3
Ivan <krash577388@mail.ru>
2022-05-18 13:28:15 +0500

    create input

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,11 @@
-import {throttle} from 'throttle-debounce';
-import {downloadAssets, getAsset} from "./assets";
-import {play, updateDirection} from "./networking";
+
+import {downloadAssets} from "./assets";
+import {play} from "./networking";
+import {startCapturingInput} from "./input";
 
 const settings = require('../settings');
 
 await downloadAssets();
-
 
 document.getElementById("play-button").onclick = () => {
     console.log("click!");
@@ -29,31 +29,4 @@ document.getElementById("modal__close-button").onclick = () => {
     document.getElementById("modal").style.display = 'none';
 };
 
-function onMouseInput(e) {
-    handleInput(e.clientX, e.clientY);
-}
 
-function onTouchInput(e) {
-    const touch = e.touches[0];
-    handleInput(touch.clientX, touch.clientY);
-}
-
-function handleInput(x, y) {
-    const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-    console.log(dir);
-    updateDirection(dir);
-}
-
-function startCapturingInput() {
-    window.addEventListener('mousemove', onMouseInput);
-    window.addEventListener('click', onMouseInput);
-    window.addEventListener('touchstart', onTouchInput);
-    window.addEventListener('touchmove', onTouchInput);
-}
-
-function stopCapturingInput() {
-    window.removeEventListener('mousemove', onMouseInput);
-    window.removeEventListener('click', onMouseInput);
-    window.removeEventListener('touchstart', onTouchInput);
-    window.removeEventListener('touchmove', onTouchInput);
-}
---
+++ src/client/render.js
@@ -0,0 +1,40 @@
+import { getAsset } from './assets';
+import { getCurrentState } from './state';
+
+const Constants = require('../shared/constants');
+const { PLAYER_RADIUS, PLAYER_MAX_HP, BULLET_RADIUS, MAP_SIZE } = Constants;
+
+// Get the canvas graphics context
+const canvas = document.getElementById('game-canvas');
+const context = canvas.getContext('2d');
+
+// Make the canvas fullscreen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function render() {
+    const { me, others, bullets } = getCurrentState();
+    if (!me) {
+        return;
+    }
+
+    // Draw background
+    renderBackground(me.x, me.y);
+
+    // Draw all bullets
+    bullets.forEach(renderBullet.bind(null, me));
+
+    // Draw all players
+    renderPlayer(me, me);
+    others.forEach(renderPlayer.bind(null, me));
+}
+
+// ... Helper functions here excluded
+
+let renderInterval = null;
+export function startRendering() {
+    renderInterval = setInterval(render, 1000 / 60);
+}
+export function stopRendering() {
+    clearInterval(renderInterval);
+}
