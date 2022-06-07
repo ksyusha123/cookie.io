5b99ccfc01743ef67f3ccc703f40eca5cc03b23d
Ivan <krash577388@mail.ru>
2022-06-07 16:14:32 +0500

    fix skin menu

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-import {downloadAssets, getAsset, downloadSkinMenuAssets} from "./assets";
+import {downloadAssets, getPersonAsset, downloadSkinMenuAssets, updateSkinButton} from "./assets";
 import {play} from "./networking";
 import {startCapturingInput} from "./input";
 import {startRendering} from "./render";
@@ -8,6 +8,7 @@ const settings = require('../settings');
 await downloadAssets();
 
 let skin = 'Zhenya.png';
+updateSkinButton(skin);
 
 const headMenu = document.getElementsByClassName('head-menu')[0];
 const choseMenu = document.getElementsByClassName('chose-menu')[0];
@@ -35,6 +36,15 @@ document.getElementById("modal__close-button").addEventListener('click', () => {
     headMenu.style.display = 'flex';
     choseMenu.style.display = 'flex';
     document.getElementById("modal").style.display = 'none';
+    updateSkinButton(skin);
 });
+
+for (let e of document.getElementById('modal-skins').childNodes){
+    if (e.nodeType !== 1)
+        continue;
+    e.addEventListener('click', () => {
+        skin = getPersonAsset(Number(e.id.slice(4)) - 1);
+    });
+}
 
 
