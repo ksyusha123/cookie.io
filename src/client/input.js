58c34bf79b84f91135f92e4d447208c5f1c40edc
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-06-19 19:39:02 +0500

    fix for double tap

--- src/client/input.js
+++ src/client/input.js
@@ -4,7 +4,7 @@ const MIN_SPEED_DEVIATION_FOR_NON_GAMEPAD = 25;
 const MAX_SPEED_DEVIATION_FOR_NON_GAMEPAD = 150;
 const MIN_SPEED_DEVIATION_FOR_GAMEPAD = 0.1;
 const MAX_SPEED_DEVIATION_FOR_GAMEPAD = 1;
-const DOUBLE_TAP_MAX_INTERVAL = 800;
+const DOUBLE_TAP_MAX_INTERVAL = 500;
 
 const animationFrame = window.mozRequestAnimationFrame || window.requestAnimationFrame;
 
@@ -25,6 +25,7 @@ function onTouchInput(e) {
         splitPlayer();
     }
     previousTouchTime = touchTime;
+    return false;
 }
 
 function onGamepadUpdate() {
